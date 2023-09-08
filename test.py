4fc6d451bff5e29887c4a34cbba73661ce40e089
ID <ivandiabin@gmail.com>
2023-09-08 19:54:29 +0200

    test

---
+++ test.py
@@ -0,0 +1,15 @@
+a=2
+b=3
+c=a+b
+c=a**b
+c=b**a
+print(c)# test оператори(+-*/) та операнди (а.b.c)
+age = 25
+print("my age = "+ str (age))# переменная 'age'
+
+x = 2
+y = x +10
+print (y)
+input('what is your name?') # input - строка запрошення Інтерпретатор буде чекати до тих пір, доки не зустріне символ нового рядка (Enter). Після цього весь введений з клавіатури текст буде повернений як результат роботи функції input.
+age = input ('How old are you?')
+print ('You are '+ str (age) + ' years old.')
